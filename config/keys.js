ab11cf03fe6865a660a93c28e8fe5d12cb32ccd8
MIKE <mike.santoro42@gmail.com>
2019-01-01 16:02:00 -0500

    connecting to db (ipwhitelist fixed)

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
     MongoURI: 'mongodb://msanto7:mikeroot@auth-cluster-shard-00-00-o7s32.mongodb.net:27017,auth-cluster-shard-00-01-o7s32.mongodb.net:27017,auth-cluster-shard-00-02-o7s32.mongodb.net:27017/test?ssl=true&replicaSet=auth-cluster-shard-0&authSource=admin&retryWrites=true'
 }
+
+//transient transaction error was caused by IP whitelist (did not allow any IP)
